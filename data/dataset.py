780d1d69c66bb00502d82495429bb675f2654621
윤상훈 <shygiants@gmail.com>
2017-05-29 01:00:59 +0900

    Applied sharding

--- data/dataset.py
+++ data/dataset.py
@@ -1,34 +1,50 @@
-from tensorflow.contrib.learn.python.learn.datasets import base
-import tensorflow as tf
 import os
-from datetime import datetime, timedelta
-import numpy
+import sys
 import json
 import operator
 import re
 import math
 import random
+import threading
+
+import tensorflow as tf
+import numpy as np
+
+from datetime import datetime, timedelta
 from textblob import TextBlob as tb
 
 word_counter = {}
 longest_topic = 0
 
-flags = tf.app.flags
-FLAGS = flags.FLAGS
+tf.app.flags.DEFINE_integer('train_shards', 32,
+                            'Number of shards in training TFRecord files.')
+tf.app.flags.DEFINE_integer('validation_shards', 4,
+                            'Number of shards in validation TFRecord files.')
+tf.app.flags.DEFINE_integer('test_shards', 4,
+                            'Number of shards in test TFRecord files.')
+
+tf.app.flags.DEFINE_integer('num_threads', 4,
+                            'Number of threads to preprocess the images.')
+FLAGS = tf.app.flags.FLAGS
+
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
+
 def _float_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
 
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
 
+
 def _byte_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def getWords(text):
+
+def get_words(text):
     return re.compile('\w+').findall(text)
 
 
@@ -46,7 +62,7 @@ class Topic:
                 j = json.loads(l)
                 text = j['text'] #.decode()
                 self.parse_data.append((datetime.strptime(j['created_at'], "%a %b %d %H:%M:%S +0000 %Y"), text))
-                words = getWords(text)
+                words = get_words(text)
                 for word in words:
                     if word in word_counter:
                         word_counter[word] += 1
@@ -95,14 +111,15 @@ class Topic:
             for w in data:
                 if w in vector:
                     vector[w] += data[w]
-            features.append(numpy.array(list(vector.values())).reshape((1, 5000)))
+            features.append(np.array(list(vector.values())).reshape((1, 5000)))
             counter += 1
 
         return features, counter, self.label, self.file_path
 
-# read data from files in directory
+
 def read_data_sets():
-    topics = [] # array of topics
+    """read data from files in directory"""
+    topics = []  # array of topics
     num_topic = 0
 
     for dirname, dirnames, filenames in os.walk('..'):
@@ -143,23 +160,97 @@ def read_data_sets():
     for t in topics:
         test.append(t)
 
-    write_tfrecord(train, "train")
-    write_tfrecord(validation, "valid")
-    write_tfrecord(test, "test")
+    write_tfrecord(train, "train", FLAGS.train_shards)
+    write_tfrecord(validation, "valid", FLAGS.validation_shards)
+    write_tfrecord(test, "test", FLAGS.test_shards)
 
-def write_tfrecord(topics, name):
-    # generate tf.record for train, validation, test group
-    writer = tf.python_io.TFRecordWriter("%s.tfrecords" % name)
-    for t in topics:
-        features, length, label, path = t.get_feature(longest_topic)
-        example = tf.train.Example(features=tf.train.Features(feature={
-            'tweets': _bytes_feature([f.tostring() for f in features]),
-            'length': _int64_feature(length),
-            'vector_size': _int64_feature(5000),
-            'label': _int64_feature(label),
-            'file_path': _byte_feature(tf.compat.as_bytes(path))
-        }))
-        writer.write(example.SerializeToString())
-    writer.close()
 
-read_data_sets()
+def write_tfrecord(topics, name, num_shards):
+    num_topics = len(topics)
+
+    # Break all topics into batches with a [ranges[i][0], ranges[i][1]].
+    spacing = np.linspace(0, num_topics, FLAGS.num_threads + 1).astype(np.int)
+    ranges = []
+    threads = []
+    for i in range(len(spacing) - 1):
+        ranges.append([spacing[i], spacing[i + 1]])
+
+    # Launch a thread for each batch.
+    print('Launching %d threads for spacings: %s' % (FLAGS.num_threads, ranges))
+    sys.stdout.flush()
+
+    # Create a mechanism for monitoring when all threads are finished.
+    coord = tf.train.Coordinator()
+
+    threads = []
+    for thread_index in range(len(ranges)):
+        args = (thread_index, ranges, name, topics, num_shards)
+        t = threading.Thread(target=batch_write_tfrecord, args=args)
+        t.start()
+        threads.append(t)
+
+    # Wait for all the threads to terminate.
+    coord.join(threads)
+    print('%s: Finished writing all %d topics in data set.' %
+          (datetime.now(), num_topics))
+    sys.stdout.flush()
+
+
+def batch_write_tfrecord(thread_index, ranges, name, topics, num_shards):
+    # Each thread produces N shards where N = int(num_shards / num_threads).
+    # For instance, if num_shards = 128, and the num_threads = 2, then the first
+    # thread would produce shards [0, 64).
+    num_threads = len(ranges)
+    assert not num_shards % num_threads
+    num_shards_per_batch = int(num_shards / num_threads)
+
+    shard_ranges = np.linspace(ranges[thread_index][0],
+                               ranges[thread_index][1],
+                               num_shards_per_batch + 1).astype(int)
+    num_files_in_thread = ranges[thread_index][1] - ranges[thread_index][0]
+
+    counter = 0
+    for s in range(num_shards_per_batch):
+        # Generate a sharded version of the file name, e.g. 'train-00002-of-00010'
+        shard = thread_index * num_shards_per_batch + s
+        output_filename = '%s-%.5d-of-%.5d' % (name, shard, num_shards)
+        output_file = os.path.join(FLAGS.output_directory, output_filename)
+        writer = tf.python_io.TFRecordWriter(output_file)
+
+        shard_counter = 0
+        files_in_shard = np.arange(shard_ranges[s], shard_ranges[s + 1], dtype=int)
+        for i in files_in_shard:
+            topic = topics[i]
+            features, length, label, path = topic.get_feature(longest_topic)
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'tweets': _bytes_feature([f.tostring() for f in features]),
+                'length': _int64_feature(length),
+                'vector_size': _int64_feature(5000),
+                'label': _int64_feature(label),
+                'file_path': _byte_feature(tf.compat.as_bytes(path))
+            }))
+
+            writer.write(example.SerializeToString())
+            shard_counter += 1
+            counter += 1
+
+            if not counter % 1000:
+                print('%s [thread %d]: Processed %d of %d images in thread batch.' %
+                      (datetime.now(), thread_index, counter, num_files_in_thread))
+                sys.stdout.flush()
+
+        writer.close()
+        print('%s [thread %d]: Wrote %d images to %s' %
+              (datetime.now(), thread_index, shard_counter, output_file))
+        sys.stdout.flush()
+        shard_counter = 0
+    print('%s [thread %d]: Wrote %d images to %d shards.' %
+          (datetime.now(), thread_index, counter, num_files_in_thread))
+    sys.stdout.flush()
+
+
+def main(_):
+    read_data_sets()
+
+if __name__ == 'main':
+    tf.app.run()
